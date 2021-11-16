aa38019afed114e82cbfd5e0481656d10ad114f1
JakubOchnik <jakub.ochnik1@gmail.com>
2021-11-16 20:17:32 +0100

    Add comments

--- driver.c
+++ driver.c
@@ -3,34 +3,40 @@
 // The structure representing the System Service Table.
 typedef struct SystemServiceTable
 {
-	PULONG ServiceTableBase;
-	PULONG ServiceCounterTableBase;
-	ULONG NumberOfServices;
-	PUCHAR ParamTableBase;
+	PULONG ServiceTableBase; // Array of pointers to functions
+	PULONG ServiceCounterTableBase; // Not used
+	ULONG NumberOfServices; // Number of entries in the array
+	PUCHAR ParamTableBase; // Array, each byte = num of bytes allocated for func arguments (there's an entry for each function)
 }SST, * PSST;
 
 // Tell the compiler that a specified function is exported by the kernel
-// so that it shouldn't throw an error when using it.
-// Declaration of KeServiceDescriptorTable which is exported by ntoskrnl.exe
+// (ntoskrnl.exe) so that it won't throw an error when using it.
 extern PSST KeServiceDescriptorTable;
 
-// Get the index of a service in the table
-#define GetServiceNumber(Function)(*(PULONG)((PUCHAR)Function+1));
+// Get the index of a service in SSDT
+/*
+ * Clever trick from "The Rootkit Arsenal".
+ * This macro identifies the number of the system call we're trying to hook.
+ * All of the Zw* () routines start with: mov eax, xxxh, where "xxx" is a syscall number.
+ * So, the number is actually stored in the second byte of the ADDRESS of system call.
+ * So, by adding 1 to the system call address, we can access the system call number.
+ */
+#define GetServiceNumber(Function)(*(PULONG)((PUCHAR)Function + 1));
 
-// Define the original prototype of the hooked function
+ // Define the original prototype of the hooked function
 typedef NTSTATUS(*pNtTerminateProcess)(HANDLE, NTSTATUS);
 
-// Original func addresses
-ULONG OrigNtTerminateProcess, SSDTAddress;
+// Original address of address of function in SSDT
+ULONG SSDTAddress;
 
-// Original function pointer
-pNtTerminateProcess fnNtTerminateProcess = NULL;
+// Original function handle
+pNtTerminateProcess NtTerminateProcessOrigHandle = NULL;
 
-// Hooked function
+// Hook function
 NTSTATUS HookNtTerminateProcess(HANDLE hProcess, NTSTATUS ExitStatus)
 {
 	DbgPrint("Hello, it's me!\n");
-	return fnNtTerminateProcess(hProcess, ExitStatus);
+	return NtTerminateProcessOrigHandle(hProcess, ExitStatus);
 }
 
 // Utility: Enable or disable WP bit in CR0 register
@@ -54,15 +60,14 @@ void DisableWP()
 	}
 }
 
-void Unload(IN PDRIVER_OBJECT DriverObject)
+void Unload(PDRIVER_OBJECT DriverObject)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 
-
-	if (fnNtTerminateProcess != NULL) {
+	if (NtTerminateProcessOrigHandle != NULL) {
 		DisableWP();
-		// Revert the original function address
-		InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)fnNtTerminateProcess);
+		// Revert to the original function address
+		InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)NtTerminateProcessOrigHandle);
 		EnableWP();
 		DbgPrint("The original SSDT function has been restored\n");
 	}
@@ -70,24 +75,35 @@ void Unload(IN PDRIVER_OBJECT DriverObject)
 	DbgPrint("Driver unloaded\n");
 }
 
-
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
 	UNREFERENCED_PARAMETER(RegistryPath);
+
+	// Assign the function which will be executed on driver exit
 	DriverObject->DriverUnload = Unload;
 
-	ULONG ServiceNumber = GetServiceNumber(ZwTerminateProcess);
+	// Get the INDEX of the service in SSDT table using a simple hack
+	// When Windows calls the system routine (sysenter instruction), the system call number is
+	// stored in the EAX register. In our case it's just the ZwTerminateProcess.
+	// Lower 12 bits - index in SSDT, 13th bit - SDT (KeServiceDescriptorTable [0x0-0xFFF numbers] or ...Shadow [0x1000 - 0x1FFF])
+	const ULONG RoutineIndex = GetServiceNumber(ZwTerminateProcess);
 	DisableWP();
 
-	SSDTAddress = (ULONG)KeServiceDescriptorTable->ServiceTableBase + ServiceNumber * 4;
-	OrigNtTerminateProcess = *(PULONG)SSDTAddress;
+	// Calculate the address of address of the routine held in SSDT :)
+	// addr = KeServiceTableAddress + routine_index * 4 (bytes, address length)
+	SSDTAddress = (ULONG)KeServiceDescriptorTable->ServiceTableBase + RoutineIndex * 4;
+	// Get the absolute address of original routine
+	const ULONG OrigNtTerminateProcess = *(PULONG)SSDTAddress;
 
-	// Save the address of an original function
-	fnNtTerminateProcess = (pNtTerminateProcess)OrigNtTerminateProcess;
+	// Save the address of original function
+	NtTerminateProcessOrigHandle = (pNtTerminateProcess)OrigNtTerminateProcess;
 	// Swap the original address with the hooked function address
+	// Use InterlockedExchange, because it's safer
 	InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)HookNtTerminateProcess);
 
+	// Turn the WP back on
 	EnableWP();
+
 	DbgPrint("End of DriverEntry!\n");
 
 	return STATUS_SUCCESS;
